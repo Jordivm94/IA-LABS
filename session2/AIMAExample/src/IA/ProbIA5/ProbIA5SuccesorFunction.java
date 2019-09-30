7c6b29bda5ab0f603ed63765ffadcc51ba2635a0
Jordivm94 <47102381+Jordivm94@users.noreply.github.com>
2019-09-30 19:22:24 +0200

    Update ProbIA5SuccesorFunction.java
    Verifica Alex cuando puedas este fichero.
    Saludos

--- session2/AIMAExample/src/IA/ProbIA5/ProbIA5SuccesorFunction.java
+++ session2/AIMAExample/src/IA/ProbIA5/ProbIA5SuccesorFunction.java
@@ -14,6 +14,12 @@ public class ProbIA5SuccesorFunction implements SuccessorFunction{
         ArrayList retval = new ArrayList();
         ProbIA5Board board = (ProbIA5Board) state;
 
+        for (int i = 0; i < 5; i++){
+            ProbIA5Board newBoard = new ProbIA5Board.copia_estado(board);
+            newBoard.flip_it(i);
+            if (i < 4) retval.add(new Successor(new String ("flip" + i +" "+ i+1)),newBoard);
+            else retval.add(new Successor(new String ("flip" + i +" "+ "0")),newBoard);
+        }
         // Some code here
         // (flip all the consecutive pairs of coins and generate new states
         // Add the states to retval as Succesor("flip i j", new_state)
